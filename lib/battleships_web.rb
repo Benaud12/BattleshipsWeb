123b1c4dff80dbba7e386ebebb89592a95b62b87
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-09-15 11:27:55 +0100

    version 1

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,8 +1,16 @@
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
+  set :views, proc { File.join(root, '..', 'views') }
+
   get '/' do
     'Hello BattleshipsWeb!'
+    erb :index
+  end
+
+  get '/askname' do
+    'What is your name?'
+    erb :index
   end
 
   # start the server if ruby file executed directly
